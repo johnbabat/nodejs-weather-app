849e64dd2c23ebf53f57d999f6d6e8ed3f0e24ea
John Babatola <johnbabatola@gmail.com>
2020-01-30 13:22:55 +0700

    More mobile friendly

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Weather',
+        title: 'Help',
         name: 'John Babatola'
     })
 })
